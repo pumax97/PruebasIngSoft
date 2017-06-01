3246e912e201dab401a569a3ba0c01332acfa1ac
pumax97 <davilillo1997@gmail.com>
2017-06-01 21:22:50 +0200

    Primer prueba caja negra

--- Test/ejercicioTest/MinMaxTestCajaBlanca.java
+++ Test/ejercicioTest/MinMaxTestCajaBlanca.java
@@ -1,5 +1,22 @@
 package ejercicioTest;
 
-public class MinMaxTestCajaBlanca {
+import org.junit.Before;
+import org.junit.Test;
 
+import ejercicio.MinMax;
+
+public class MinMaxTestCajaBlanca {
+	int [] xs;
+	
+	@Before
+	
+	public void inicializar(){
+		xs = new int[] {1, -8, 10, 5, -1, 0};// array para statementCoverage
+	}
+	
+	@Test
+	
+	public void statementCoverage(){
+		MinMax.minMax(xs);//pasa por todas las sentencias
+	}
 }
