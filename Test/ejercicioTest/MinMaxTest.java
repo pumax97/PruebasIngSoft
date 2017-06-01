1066874c2974aa8b594f588181fc48bb8c419f74
pumax97 <davilillo1997@gmail.com>
2017-06-01 14:13:09 +0200

    Tercera prueba: array con varios valores

--- Test/ejercicioTest/MinMaxTest.java
+++ Test/ejercicioTest/MinMaxTest.java
@@ -9,14 +9,16 @@ import ejercicio.MinMax;
 
 
 public class MinMaxTest {
-	public int [] xs, xt, xp;
+	public int [] xs, xt, xp, xz, xw;
 	
 	@Before
 	
 	public void inicializar(){
 		xs = null; //array nulo
 		xt = new int[] {1}; //array con un único valor
-		xp = new int[] {1, 1};
+		xp = new int[] {1, 1}; // array esperado para xt
+		xz = new int[] {1, 2, 3, 4, 5}; //array con varios valores
+		xw = new int[] {1, 5};// array esperado para xz
 	}
 	
 	@Test
@@ -29,5 +31,11 @@ public class MinMaxTest {
 	
 	public void arrayUnValor(){
 		assertArrayEquals("Fallo en la funcion con un unico valor", xp, MinMax.minMax(xt));
+	}
+	
+	@Test
+	
+	public void arrayVariosValores(){
+		assertArrayEquals("Fallo en la funcion con varios valores", xw, MinMax.minMax(xz));
 	}
 }
