cde07e571292d0391505f6f0186c2b06409eb9c0
pumax97 <davilillo1997@gmail.com>
2017-06-01 21:31:52 +0200

    Prueba caja negra decisiones

--- Test/ejercicioTest/MinMaxTestCajaBlanca.java
+++ Test/ejercicioTest/MinMaxTestCajaBlanca.java
@@ -6,17 +6,25 @@ import org.junit.Test;
 import ejercicio.MinMax;
 
 public class MinMaxTestCajaBlanca {
-	int [] xs;
+	int [] xs, xt;
 	
 	@Before
 	
 	public void inicializar(){
 		xs = new int[] {1, -8, 10, 5, -1, 0};// array para statementCoverage
+		xt = null;
 	}
 	
 	@Test
 	
 	public void statementCoverage(){
 		MinMax.minMax(xs);//pasa por todas las sentencias
+	}
+	
+	@Test
+	
+	public void decisionCoverage(){
+		MinMax.minMax(xs);//decision verdadera
+		MinMax.minMax(xt);//decision falsa
 	}
 }
